f02b53437661bfb6a4004bdefc3763d1dd29e849
Nikitos-7888 <156174842+Nikitos-7888@users.noreply.github.com>
2024-08-05 17:34:40 +0300

    ConsoleApp12

---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1;
+
+
+
+            for (int i = 1; i < 10; i++)
+            {
+                for (int j = 1; j < 10; j++)
+                {
+                    a = i * j;
+                    if (a >= 10) {Console.Write("    " + a); }
+                    else {Console.Write("     " + a); }
+                }
+                Console.WriteLine("");
+            }
+        }
+    }
+}
