186213ecb2bf944b1ceb51a1e81ccef6ffb2fe87
Nikitos-7888 <156174842+Nikitos-7888@users.noreply.github.com>
2024-08-05 17:34:06 +0300

    ConsoleApp10

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int a = int.Parse(Console.ReadLine());
+            int b = a * 10;
+            Console.WriteLine(b);
+        }
+    }
+}
