4692d6f7df11a3d5589544a3b7dc5fb6964254bb
Nikitos-7888 <156174842+Nikitos-7888@users.noreply.github.com>
2024-08-05 17:33:54 +0300

    ConsoleApp9

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите число 1");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine(a);
+
+            int integerPart = (int)a;
+            Console.WriteLine(integerPart);
+
+        }
+
+
+    }
+}
