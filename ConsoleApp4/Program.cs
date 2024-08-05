a25577740b83acf02461c566904f52bd749b7704
Nikitos-7888 <156174842+Nikitos-7888@users.noreply.github.com>
2024-08-05 17:32:38 +0300

    ConsoleApp4

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите число 1");
+            int a = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите число 2");
+            //int b = int.Parse(Console.ReadLine());
+
+            if (a >= 0 && a % 2 == 0)
+            {
+                Console.WriteLine("Число положительное и четное");
+            }
+            if (a < 0 || a % 2 != 0)
+            {
+                Console.WriteLine("Число отрицательное или не четное");
+            }
+        }
+    }
+}
