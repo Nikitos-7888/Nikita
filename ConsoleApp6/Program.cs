60d3146e160b60ae3d8a129b9f186bc2f24325aa
Nikitos-7888 <156174842+Nikitos-7888@users.noreply.github.com>
2024-08-05 17:33:09 +0300

    ConsoleApp6

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите число 1");
+            int a = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите число 2");
+            //int b = int.Parse(Console.ReadLine());
+
+            if (a >= 90)
+            {
+                Console.WriteLine("Оценка A");
+            }
+            if (a >= 80 && a < 90)
+            {
+                Console.WriteLine("Оценка B");
+            }
+            if (a >= 70 && a < 80)
+            {
+                Console.WriteLine("Оценка C");
+            }
+            if (a >= 60 && a < 70)
+            {
+                Console.WriteLine("Оценка D");
+            }
+            if (a < 60)
+            {
+                Console.WriteLine("Оценка F");
+            }
+        }
+    }
+}
