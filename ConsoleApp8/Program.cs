f9fd9233ec4ace83c8061291897d4143ff7ec573
Nikitos-7888 <156174842+Nikitos-7888@users.noreply.github.com>
2024-08-05 17:33:39 +0300

    ConsoleApp8

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int a = 0;
+
+            while (a < 50)
+            {
+                if (a % 2 == 0)
+                {
+                    Console.WriteLine(a);
+                }
+                a++;
+            }
+        }
+    }
+}
