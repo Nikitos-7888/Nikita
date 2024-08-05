76df323326950c2c062ab5ee9b47d22b8d0ca83b
Nikitos-7888 <156174842+Nikitos-7888@users.noreply.github.com>
2024-08-05 17:32:55 +0300

    ConsoleApp5

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите число 1");
+            int a = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите число 2");
+            //int b = int.Parse(Console.ReadLine());
+
+
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("Число четное");
+            }
+            else
+            {
+                Console.WriteLine("Число не четное");
+            }
+        }
+    }
+}
