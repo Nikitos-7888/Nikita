05766b12c5bb85ab34f4f11b8e38ba333144d0ad
Nikitos-7888 <156174842+Nikitos-7888@users.noreply.github.com>
2024-08-05 17:32:21 +0300

    ConsoleApp3

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите число 1");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите число 2");
+            int b = int.Parse(Console.ReadLine());
+
+            if (a == b)
+            {
+                Console.WriteLine("a равно b");
+            }
+            if (a > b)
+            {
+                Console.WriteLine("a больше b");
+            }
+            if (a < b)
+            {
+                Console.WriteLine("a меньше b");
+            }
+        }
+    }
+}
