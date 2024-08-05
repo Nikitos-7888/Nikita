63303ef757f4577576696b234b5d6ce05339b12c
Nikitos-7888 <156174842+Nikitos-7888@users.noreply.github.com>
2024-08-05 17:33:25 +0300

    ConsoleApp7

---
+++ ConsoleApp7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число 1");
+            int a = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Введите число 2");
+            //int b = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < a; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+     }
+}
