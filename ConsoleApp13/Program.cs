f2fb222ccde3336a4aa34ea9993d06056be1eb61
Nikitos-7888 <156174842+Nikitos-7888@users.noreply.github.com>
2024-08-05 17:35:00 +0300

    ConsoleApp13

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите год");
+            int year = int.Parse(Console.ReadLine());
+            Console.WriteLine(year);
+
+            if (year % 4 == 0 && year % 100 != 0)
+            {
+                Console.WriteLine("Високосный год");
+            }
+            else
+            {
+                Console.WriteLine("Не Високосный год");
+            }
+        }
+    }
+}
