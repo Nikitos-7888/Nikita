6dc3e254631edadb6e5bf0cfe93e38dbdcfa5bec
Nikitos-7888 <156174842+Nikitos-7888@users.noreply.github.com>
2024-08-05 17:31:40 +0300

    ConsoleApp1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите строку");
+            string a = Console.ReadLine();
+            Console.WriteLine(a);
+
+            Console.WriteLine("Введите число");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine(b);
+
+            Console.WriteLine("Введите число с плавующей точкой");
+            double c = double.Parse(Console.ReadLine());
+            Console.WriteLine(c);
+
+            Console.WriteLine("Введите символ");
+            char d = char.Parse(Console.ReadLine());
+            Console.WriteLine(d);
+
+            Console.WriteLine("Введите логическое значение");
+            bool e = bool.Parse(Console.ReadLine());
+            Console.WriteLine(e);
+
+        }
+    }
+}
