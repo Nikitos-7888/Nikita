86a3fe2554cc426b07121247656671c0f9c6b49a
Nikitos-7888 <156174842+Nikitos-7888@users.noreply.github.com>
2024-08-05 17:34:24 +0300

    ConsoleApp11

---
+++ ConsoleApp11/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int a = int.Parse(Console.ReadLine());
+            int w = 1;
+            for (int i = 1; i <= a; i++)
+            {
+                w = w * i;
+            }
+            Console.WriteLine(w);
+        }
+    }
+}
